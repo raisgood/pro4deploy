475414ff813d1aaa0f8cba8fecc8e30b4f120766
Rashad Humphrey <rashadthecoder@gmail.com>
2021-03-24 13:42:46 -0500

    initial commit

---
+++ django_environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rasha\webdev\pro4deploy\django_environment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
