b0a62ac7b8ec1f8ec6455ac82758404195e9efc2
Rashad Humphrey <rashadthecoder@gmail.com>
2021-03-25 14:37:14 -0500

    commit Development folder

--- Development/bookstore/bookstore/settings.py
+++ Development/bookstore/bookstore/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't%h3c64buoj#m__34y_%d(1sj@u=ip^b8d6hvc15v+dqrm1)^g'
+SECRET_KEY = os.environ.get('PRO4DEPLOY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Activate Django_heroku
+django_heroku.settings(locals())
