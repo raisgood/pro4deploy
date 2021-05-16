ce10671b5d34d61ba43cef9d63b6fb5ea337b2f7
Rashad Humphrey <rashadthecoder@gmail.com>
2021-05-16 08:09:01 -0500

    add for deployment

--- Development/bookstore/bookstore/settings.py
+++ Development/bookstore/bookstore/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('PRO4DEPLOY_KEY')
-SECRET_KEY = 'testing'
+SECRET_KEY = os.environ.get('PRO4DEPLOY_KEY')
+#SECRET_KEY = 'testing'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
